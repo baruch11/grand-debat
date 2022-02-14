3101aed83621d45786114666a4eb8a419c006b3f
chp <charprat@yahoo.fr>
2022-02-14 16:46:28 +0100

    implement get_topic_by_relevance

--- grand_debat/theme_detection.py
+++ grand_debat/theme_detection.py
@@ -1,5 +1,6 @@
 import pyLDAvis
 import pyLDAvis.sklearn
+import numpy as np
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.feature_extraction.text import CountVectorizer
 import warnings
@@ -22,3 +23,24 @@ def get_themes(data_theme_response_dict, selected_question, n_topics=20, max_ite
     pyLDAvis.save_html(pyLDAVIS_tf, 'pyLDAVIS_tf.html')
 
     return tf_vectorizer, lda_tf
+
+
+def get_topic_by_relevance(vectorizer, lda, lambd, n_topic, n_top=10):
+    """Get the topics from a sklearn countvectorizer and lda sorted by relevance
+    see def of relevance in pyLDAvis article
+    https://nlp.stanford.edu/events/illvi2014/papers/sievert-illvi2014.pdf
+
+    Args:
+        vectorizer: sklearn CountVectorizer
+        lda: sklearn LatentDirichletAllocation
+        lambd: float, lambda in pyLDAvis paper
+        n_topic: int, indice of the topic
+        n_top: nomber of top words for topic representation
+    Returns:
+        list of strings, topic words set representation
+    """
+    feature_names = vectorizer.get_feature_names_out()
+    pw = lda.components_.sum(axis=0) / lda.components_.sum()
+    pw_t = lda.components_ / lda.components_.sum(axis=1)[:, np.newaxis]
+    relevance = lambd * pw_t + (1 - lambd) * pw_t / pw
+    return feature_names[np.argsort(relevance[n_topic, :])[:-n_top:-1]]
